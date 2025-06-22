2f6074762d7fee0a281011d81db697921a7a4693
Escalipt12 <escalipt@gmail.com>
2025-06-22 20:30:14 +1000

    Update main.cpp

--- FindWindowA/main.cpp
+++ FindWindowA/main.cpp
@@ -1,1 +1,18 @@
 
+#include <Windows.h>
+#include <iostream>
+
+
+
+void main(){
+	std::string Fedya = "dyrak";
+	HWND window = FindWindowA("Notepad" , "Текстовый документ (2).txt — Блокнот");
+	if (window) {
+		std::cout << "fedya" << std::endl;
+		SetWindowTextA(window, "FEDKA");
+	}
+	if (FindWindowA("Notepad", "FEDKA")) {
+		std::cout << "fff" << std::endl;
+	}
+
+}
